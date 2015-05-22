be651f28590123dd1b15cf07fbaf5855333070c3
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-05-21 21:11:38 -0500

    all buttons working

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ var info = "http://www.omdbapi.com/?t=";
 var title = $('.movie').val();
 var url = info + title;
 var FIREBASE_URL = "https://flickpicker.firebaseio.com/flicks.json"
-var FIREBASE_AUTH = 'https://buildscripts.firebaseio.com/';
+var FIREBASE_AUTH = 'https://flickpicker.firebaseio.com/';
 var fb = new Firebase(FIREBASE_AUTH);
 
 $('.search').on('click', function() {
@@ -26,7 +26,7 @@ function addMovieDetail(data, id){
   $target.append("<td>" + data.imdbRating + "</td>");
   $target.append("<td>" + data.Year + "</td>");
   $target.append("<td><img width='200' height='250' src=" + data.Poster + "></img></td>");
-  $target.append("<button class='delete'>Delete</button>");
+  $target.append("<button class='delete button-primary'>Delete</button>");
 };
 
 $.get(FIREBASE_URL, function (movies) {
@@ -53,9 +53,12 @@ $button.on("click", ".delete", function() {
 //Im not sure how to add the data ID to each td and then select that id like im trying to do in lines 38 and 39
 //starting testing for firebase logns wed may 20th
 //everything below this point is for login.html
+
+//login button
 $('.loginPageForm form').on('submit', function (evt) {
   var email = $('.loginPageForm input[type="email"]').val();
   var password = $('.loginPageForm input[type="password"]').val();
+  var onTempPassword = $('.onTempPassword');
   evt.preventDefault();
   fb.authWithPassword({
     email: email,
@@ -63,7 +66,9 @@ $('.loginPageForm form').on('submit', function (evt) {
   }, function (err, authData) {
     if (err) {
       alert(err.toString());
-    } else  {
+    } else if(authData && authData.password.isTemporaryPassword) {
+      onTempPassword.removeClass('hidden');
+    } else {
       //cb(authData);
       console.log("logged in")
       window.location = '/'
@@ -71,14 +76,117 @@ $('.loginPageForm form').on('submit', function (evt) {
   });
 });
 
-//working on having it land on the login page if not logged in
-// if (window.location.pathname !== '/login/login && !fb.getAuth()) {
-//  window.location ='/login';
-// }
-$( document ).ready(function() {
-  debugger;
-if (window.location.pathname !== '/login.html' && !fb.getAuth()) {
-  console.log("Hello");
- window.location ='/login.html';
-  }
+// runs when called by registration button if  successful
+function doLogin (email, password, cb) {
+  fb.authWithPassword({
+    email: email,
+    password: password
+  }, function (err, authData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      saveAuthData(authData);
+      typeof cb === 'function' && cb(authData);
+      window.location = '/'
+      alert("Registration Successful!")
+    }
+  });
+}
+//saves the authentication data to firebase
+function saveAuthData (authData) {
+  $.ajax({
+    method: 'PUT',
+    url: `${FIREBASE_AUTH}/users/${authData.uid}/profile.json`,
+    data: JSON.stringify(authData)
+  });
+}
+
+
+//register button
+$('.loginPageForm .doRegister').click(function () {
+  var email = $('.loginPageForm input[type="email"]').val();
+  var password = $('.loginPageForm input[type="password"]').val();
+
+  fb.createUser({
+    email: email,
+    password: password
+  }, function (err, userData) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      doLogin(email, password);
+    }
+  });
+  event.preventDefault();
 });
+
+//forgot password button
+$('.loginPageForm .doResetPassword').click(function () {
+  var email = $('.loginPageForm input[type="email"]').val();
+
+  fb.resetPassword({
+    email: email
+  }, function (err) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      alert('Check your email!');
+    }
+  });
+});
+
+//reset password form
+$('.onTempPassword form').submit(function () {
+  var email = fb.getAuth().password.email;
+  var oldPw = $('.onTempPassword input:nth-child(1)').val();
+  var newPw = $('.onTempPassword input:nth-child(2)').val();
+
+  fb.changePassword({
+    email: email,
+    oldPassword: oldPw,
+    newPassword: newPw
+  }, function(err) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      fb.unauth();
+    }
+  });
+
+  event.preventDefault();
+})
+
+//reset password button
+$('.onTempPassword form').submit(function () {
+  var email = fb.getAuth().password.email;
+  var oldPw = $('.onTempPassword input:nth-child(1)').val();
+  var newPw = $('.onTempPassword input:nth-child(2)').val();
+
+  fb.changePassword({
+    email: email,
+    oldPassword: oldPw,
+    newPassword: newPw
+  }, function(err) {
+    if (err) {
+      alert(err.toString());
+    } else {
+      fb.unauth();
+      window.location = '/'
+    }
+  });
+
+  event.preventDefault();
+})
+
+//logout button
+$('.logout').on('click', function() {
+  fb.unauth();
+  window.location = '/'
+});
+
+//on page load it checks if you are logged in.  If you arent, it sends you to login page.
+$( document ).ready(function() {
+if (window.location.pathname !== '/login.html' && !fb.getAuth()) {
+ window.location ='/login.html';
+    }
+  });
