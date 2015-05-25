5ebbab45f305babadf23080f17b5c06f21303cd0
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-05-25 12:51:05 -0500

    buttons working, data persists.

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,11 @@ var info = "http://www.omdbapi.com/?t=";
 var title = $('.movie').val();
 var url = info + title;
 var FIREBASE_URL = "https://flickpicker.firebaseio.com/flicks.json"
-var FIREBASE_AUTH = 'https://flickpicker.firebaseio.com/';
+var FIREBASE_AUTH = 'https://flickpicker.firebaseio.com';
 var fb = new Firebase(FIREBASE_AUTH);
 
+
+//search button working and done
 $('.search').on('click', function() {
   var input = document.querySelector("#movieName");
   var title = input.value;
@@ -13,37 +15,52 @@ $('.search').on('click', function() {
   var token = fb.getAuth().token;
   var postUrl = `${FIREBASE_AUTH}/users/${uid}/flicks.json?auth=${token}`;
   $.get(url, function (data) {
+    if(data.Response === "True"){
     $.post(postUrl, JSON.stringify(data), function (res) {
     console.log("Post Request Passed")
   });
     addMovieDetail(data);
+    } else {
+      alert("Sorry! Film not found! Check title and try again!")
+    }
   }, 'jsonp');
+});
+
+//loads data per user and keeps its persisting
+$(document).ready(function(){
+  if (window.location.pathname === '/' && fb.getAuth() !== null){
+    var uid = fb.getAuth().uid;
+    var token = fb.getAuth().token;
+    var MovieListUrl = `${FIREBASE_AUTH}/users/${uid}/flicks.json?auth=${token}`;
+    $.get(MovieListUrl, function (movies) {
+      Object.keys(movies).forEach(function (id) {
+      addMovieDetail(movies[id], id);
+      });
+    });
+  }
 });
 
 function addMovieDetail(data, id){
   var $table=$(".table");
-  $table.append("<tr></tr>");
-  var $target=$("tr:last");
-  $target.attr("data-id", id);
-  $target.append("<td>" + data.Title + "</td>");
-  $target.append("<td>" + data.imdbRating + "</td>");
-  $target.append("<td>" + data.Year + "</td>");
-  $target.append("<td><img width='200' height='250' src=" + data.Poster + "></img></td>");
-  $target.append("<button class='delete button-primary'>Delete</button>");
+    $table.append("<tr></tr>");
+    var $target=$("tr:last");
+      $target.attr("data-id", id);
+      $target.append("<td>" + data.Title + "</td>");
+      $target.append("<td>" + data.imdbRating + "</td>");
+      $target.append("<td>" + data.Year + "</td>");
+      $target.append("<td><img width='200' height='250' src=" + data.Poster + "></img></td>");
+      $target.append("<button class='delete button-primary'>Delete</button>");
 };
 
-// $.get(FIREBASE_URL, function (movies) {
-//   Object.keys(movies).forEach(function (id) {
-// 		addMovieDetail(movies[id], id);
-//   });
-// });
-
 ///Playing with my delete on click function here
+///works.  Deletes individual users data from firebase
 var $button = $('.table');
 $button.on("click", ".delete", function() {
+  var uid = fb.getAuth().uid;
+  var token = fb.getAuth().token
   var $movieRow = $(this).closest('tr');
   var id = $movieRow.attr('data-id');
-  var deleteUrl = FIREBASE_URL.slice(0, -5) + '/' + id + '.json';
+  var deleteUrl = FIREBASE_AUTH + '/' + 'users' + '/' + uid + '/' + 'flicks' + '/' + id + '.json';
     $.ajax({
     url: deleteUrl,
     type: 'DELETE',
@@ -53,7 +70,6 @@ $button.on("click", ".delete", function() {
   })
 });
 //end playing
-//Im not sure how to add the data ID to each td and then select that id like im trying to do in lines 38 and 39
 //starting testing for firebase logns wed may 20th
 //everything below this point is for login.html
 
@@ -78,37 +94,6 @@ $('.loginPageForm form').on('submit', function (evt) {
     }
   });
 });
-///ideas for posting UID to FB.
-// $('.loginPageForm form').on('submit', function (evt) {
-//   var email = $('.loginPageForm input[type="email"]').val();
-//   var password = $('.loginPageForm input[type="password"]').val();
-//   var onTempPassword = $('.onTempPassword');
-//   evt.preventDefault();
-//   fb.authWithPassword({
-//     email: email,
-//     password: password
-//   }, function (err, authData) {
-//     if (err) {
-//       alert(err.toString());
-//     } else if(authData && authData.password.isTemporaryPassword) {
-//       onTempPassword.removeClass('hidden');
-//     } else {
-//       debugger;
-//       var title = $('.input input[type="text"]').val();
-//       var uid = authData.uid;
-//       var token = authData.token;
-//       var postUrl = `${FIREBASE_AUTH}/users/${uid}/flicks.json?auth=${token}`;
-//       $.post(postUrl, JSON.stringify(title), function (res) {
-//         console.log("Post Request Passed");
-//     //addPhotosToDom({url: url});
-//     //clearForms();
-//     // res = { name: '-Jk4dfDd123' }
-//     })
-//       console.log("logged in")
-//       window.location = '/'
-//     }
-//   });
-// });
 
 // runs when called by registration button if  successful
 function doLogin (email, password, cb) {
